736704f6b901fcb271268e2acfc9d3f8876e9054
maxime.terray <maxime.terray@gmail.com>
2019-04-23 18:15:17 +0200

    mardi 23 avril soir update

--- kTool.py
+++ kTool.py
@@ -15,6 +15,10 @@ def catchJobException(func):
     return doIt
 
 class KTool():
+
+# INITIALISATION :
+# initialisation des variables et des etats des boutons de kmaxToolBar en fonction de celles de maya
+
     def __init__(self):
         self.wscName = "kToolWorkspace"
         self.winName = "kToolWindow"
@@ -22,15 +26,18 @@ class KTool():
         self.selectColor = [0.32156, 0.52156, 0.65098]
 
         self.initPath()
-        self.scriptJobIds = []
-        self.initScriptJobs()
 
         self.buildUI()
+
+        self.scriptJobIds = []
+        self.initScriptJobs()
 
         self.actuSelection()
         self.actuTypeSelection()
         self.actuToolSettings()
+        self.initSelectStyle()
 
+# build UI
     def buildUI(self):
         if mc.window(self.winName, query=True, exists=True):
             mc.deleteUI(self.winName)
@@ -47,37 +54,37 @@ class KTool():
         mc.columnLayout('columnLayout1', height=1100, adjustableColumn=True)
 
 
-        ## Frame 'Common Selection Options'
+# build UI : Frame 'Common Selection Options'
         mc.frameLayout('selection_Layout', parent='columnLayout1', collapsable=True, label='Common Selection Options')
         mc.columnLayout('columnLayout2')
         mc.rowColumnLayout(numberOfColumns=2)
         self.lb_tool = mc.iconTextStaticLabel(width=44, visible=True, image=self.path + 'selectTool.png')
         mc.rowColumnLayout(numberOfColumns=3, columnSpacing=[[2, 1], [3, 1]])
         mc.iconTextRadioCollection()
-        self.bt_selObj = mc.iconTextRadioButton(width=44, height=32, enableBackground=False, image=self.path + 'objectnex.png', select=True)
-        self.bt_selMulti = mc.iconTextRadioButton(width=44, height=32, enableBackground=False, label='Multi',  style='textOnly')
-        self.bt_selUv = mc.iconTextRadioButton(width=44, height=32, image=self.path + 'uvnex.png')
-        self.bt_selVert = mc.iconTextRadioButton(width=44, height=32, image=self.path + 'vertexnex.png')
-        self.bt_selEdge = mc.iconTextRadioButton(width=44, height=32, image=self.path + 'edgesnex.png')
-        self.bt_selFace = mc.iconTextRadioButton(width=44, height=32, image=self.path + 'facesnex.png')
+        self.bt_selObj = mc.iconTextRadioButton(width=44, height=32, enableBackground=False, image=self.path + 'objectnex.png', select=True, onCommand=self.selObject)
+        self.bt_selMulti = mc.iconTextRadioButton(width=44, height=32, enableBackground=False, label='Multi',  style='textOnly', onCommand=self.selMulti)
+        self.bt_selUv = mc.iconTextRadioButton(width=44, height=32, image=self.path + 'uvnex.png', onCommand=self.selUv)
+        self.bt_selVert = mc.iconTextRadioButton(width=44, height=32, image=self.path + 'vertexnex.png', onCommand=self.selVertex)
+        self.bt_selEdge = mc.iconTextRadioButton(width=44, height=32, image=self.path + 'edgesnex.png', onCommand=self.selEdge)
+        self.bt_selFace = mc.iconTextRadioButton(width=44, height=32, image=self.path + 'facesnex.png', onCommand=self.selFace)
         mc.rowColumnLayout(parent='columnLayout2', numberOfColumns=4, columnSpacing=[[2, 1], [3, 1], [4, 1]])
         mc.iconTextRadioCollection()
-        mc.iconTextRadioButton(width=44, height=22, label='Marquee',  style='textOnly', font='tinyBoldLabelFont', select=True)
-        mc.iconTextRadioButton(width=44, height=22, label='Drag',  style='textOnly', font='tinyBoldLabelFont')
-        mc.iconTextRadioButton(width=44, height=22, label='Tweak',  style='textOnly', font='tinyBoldLabelFont')
-        mc.iconTextCheckBox(width=44, height=22, label='Cam B',  style='textOnly', font='tinyBoldLabelFont')
+        self.bt_marqueeSelStyle = mc.iconTextRadioButton(width=44, height=22, label='Marquee',  style='textOnly', font='tinyBoldLabelFont', select=True)
+        self.bt_dragSelStyle = mc.iconTextRadioButton(width=44, height=22, label='Drag',  style='textOnly', font='tinyBoldLabelFont')
+        self.bt_tweakSelStyle = mc.iconTextRadioButton(width=44, height=22, label='Tweak',  style='textOnly', font='tinyBoldLabelFont')
+        self.bt_camSwitch = mc.iconTextCheckBox(width=44, height=22, label='Cam B',  style='textOnly', font='tinyBoldLabelFont')
         mc.rowColumnLayout('rowColumnLayout4', parent='columnLayout2', numberOfColumns=2, columnWidth=[[1, 20], [2, 160]])
         mc.iconTextStaticLabel(width=20, height=20, image=self.path + 'transform.png')
-        self.tf_transformName = mc.textField()
+        self.tf_transformName = mc.textField(enterCommand=self.renameTransform)
         mc.iconTextStaticLabel(width=20, height=20, image=self.path + 'shape.png')
         self.tf_shapeName = mc.textField(editable=False)
         mc.iconTextStaticLabel(width=20, height=20, image=self.path + 'constructionHistory.png')
-        self.om_history = mc.optionMenu()
-        mc.iconTextStaticLabel(parent='rowColumnLayout4', width=20, height=20, image=self.path + 'quickRename.png')
-        self.tf_quickRename = mc.textField(parent='rowColumnLayout4')
+        self.om_history = mc.optionMenu(changeCommand=self.openAttributEditor, alwaysCallChangeCommand=True, maxVisibleItems=10)
+        mc.iconTextStaticLabel(width=20, height=20, image=self.path + 'quickRename.png')
+        self.tf_selByName = mc.textField(placeholderText='select by name', enterCommand=self.selectByName)
 
 
-        ## Frame 'Transform'
+# build UI : Frame 'Transform'
         mc.frameLayout('transform_Layout', parent='columnLayout1', collapsable=True, label='Transform')
         mc.columnLayout('columnLayout3')
         mc.channelBox('channelBox1', width=180, height=192, # preventOverride=False,
@@ -110,11 +117,11 @@ class KTool():
         mc.iconTextButton(enable=False, width=44, height=22, label='//',  style='textOnly')
 
 
-        ## Frame 'Display'
+# build UI : Frame 'Display'
         mc.frameLayout('display_Layout', parent='columnLayout1', collapsable=True, label='Display')
 
 
-        ## Frame 'Tool Settings'
+# build UI : Frame 'Tool Settings'
         self.fl_tool = mc.frameLayout('tool_Layout', parent='columnLayout1', collapsable=True, label='Tool Settings')
         mc.columnLayout('columnLayout6')
         mc.rowColumnLayout(numberOfColumns=4, columnSpacing=[[2, 1], [3, 1], [4, 1]])
@@ -152,7 +159,7 @@ class KTool():
         mc.iconTextCheckBox(width=44, height=22, enableBackground=True, label='Smt.Ext',  style='textOnly', font='tinyBoldLabelFont')
 
 
-        ## Frame 'Soft'
+# build UI : Frame 'Soft'
         mc.frameLayout('soft_Layout', parent='columnLayout1', collapsable=True, label='Soft')
         mc.columnLayout('columnLayout5')
         mc.rowColumnLayout('rowColumnLayout9', height=46, numberOfColumns=3, columnSpacing=[[2, 1], [3, 1]])
@@ -174,7 +181,7 @@ class KTool():
         mc.iconTextRadioButton(width=44, height=22, image=self.path + 'softPresetC.png',  style='iconOnly', font='tinyBoldLabelFont')
 
 
-        ## Frame 'Symmetry'
+# build UI : Frame 'Symmetry'
         mc.frameLayout('sym_Layout', parent='columnLayout1', collapsable=True, label='Symmetry')
         mc.columnLayout('columnLayout4')
         mc.rowColumnLayout(height=46, numberOfColumns=2)
@@ -195,7 +202,7 @@ class KTool():
         mc.floatField(width=44, height=22, precision=3)
 
 
-        ## Frame 'Step Snap'
+# build UI : Frame 'Step Snap'
         mc.frameLayout('step_Layout', parent='columnLayout1', collapsable=True, label='Step Snap')
         mc.columnLayout('columnLayout7')
         mc.rowColumnLayout(height=46, numberOfColumns=2)
@@ -221,6 +228,9 @@ class KTool():
         allowedAreas = ['left', 'right']
         mc.toolBar(self.toolBarName, area='right', content=self.winName, allowedArea=allowedAreas) # , parent=gMainWindow)
 
+
+# DEFINITION DES FONCTIONS D'INITIALISATION :
+
     def initPath(self):
         # self.path = "/homes/mte/maya/2016/scripts/kTools/icons/"
         # self.path = "E:/kTools/icons/"
@@ -239,23 +249,30 @@ class KTool():
         print "JOB ID >> :", iJob
         return mc.scriptJob(ex=iJob)
 
-    def initScriptJobs(self):
-        self.scriptJobIds.append(mc.scriptJob(event=("SelectModeChanged", catchJobException(self.actuTypeSelection))))
-        self.scriptJobIds.append(mc.scriptJob(event=("SelectTypeChanged", catchJobException(self.actuTypeSelection))))
-        self.scriptJobIds.append(mc.scriptJob(event=("SelectionChanged", catchJobException(self.actuSelection))))
-        self.scriptJobIds.append(mc.scriptJob(event=("ToolChanged", catchJobException(self.actuToolSettings))))
-        # self.scriptJobIds.append(mc.scriptJob(event=("ModelPanelSetFocus", catchJobException(self.testJob))))
-        # self.scriptJobIds.append(mc.scriptJob(event=("modelEditorChanged", catchJobException(self.initDisplay))))
+    def setIconTool(self, toolType):
+        mc.iconTextStaticLabel(self.lb_tool, edit=True, image=self.path + toolType + '.png')
 
-    def closeEvent(self, event):
-        print ">> Bye bye !"
-        try:
-            if self.scriptJobIds:
-                for jobId in self.scriptJobIds:
-                    mc.scriptJob(kill=jobId)
-                    print ">> Job {0} killed".format(jobId)
-        finally:
-            return QtGui.QWidget.closeEvent(self, event)
+    def initSelectStyle(self):
+        if mc.selectPref(q=True, paintSelect=True):
+            mc.iconTextRadioButton(self.bt_dragSelStyle, edit=True,select=True)
+            if mc.selectPref(q=True, paintSelectWithDepth=True):
+                mc.iconTextCheckBox(self.bt_camSwitch, edit=True, value=True)
+            else:
+                mc.iconTextCheckBox(self.bt_camSwitch, edit=True, value=False)
+        else:
+            mc.iconTextRadioButton(self.bt_marqueeSelStyle, edit=True, select=True)
+            if mc.selectPref(q=True, useDepth=True):
+                mc.iconTextCheckBox(self.bt_camSwitch, edit=True, value=True)
+            else:
+                mc.iconTextCheckBox(self.bt_camSwitch, edit=True, value=False)
+
+    def initScriptJobs(self):
+        self.scriptJobIds.append(mc.scriptJob(parent=self.winName, event=("SelectModeChanged", catchJobException(self.actuTypeSelection))))
+        self.scriptJobIds.append(mc.scriptJob(parent=self.winName, event=("SelectTypeChanged", catchJobException(self.actuTypeSelection))))
+        self.scriptJobIds.append(mc.scriptJob(parent=self.winName, event=("SelectionChanged", catchJobException(self.actuSelection))))
+        self.scriptJobIds.append(mc.scriptJob(parent=self.winName, event=("ToolChanged", catchJobException(self.actuToolSettings))))
+        # self.scriptJobIds.append(mc.scriptJob(parent=self.winName,event=("ModelPanelSetFocus", catchJobException(self.testJob))))
+        # self.scriptJobIds.append(mc.scriptJob(parent=self.winName,event=("modelEditorChanged", catchJobException(self.initDisplay))))
 
     def actuToolSettings(self):
         ctx = mc.currentCtx()
@@ -372,5 +389,58 @@ class KTool():
         else:
             print "pas de selection"
 
-    def setIconTool(self, toolType):
-        self.lb_tool = mc.iconTextStaticLabel(self.lb_tool, edit=True, image=self.path + toolType + '.png')
+    def openAttributEditor(self, histItem):
+        pm.mel.showEditor(histItem)
+
+    def closeEvent(self, event):
+        print ">> Bye bye !"
+        try:
+            if self.scriptJobIds:
+                for jobId in self.scriptJobIds:
+                    mc.scriptJob(kill=jobId)
+                    print ">> Job {0} killed".format(jobId)
+        finally:
+            return QtGui.QWidget.closeEvent(self, event)
+
+
+# FONCTIONS APPELE PAR LES BOUTONS DE LA "KMAX TOOL BAR"
+
+# COMMON SELECTION OPTIONS
+
+# DEFINITION DES FONCTIONS D'INTERACTIONS :
+
+# 'Common Selection Options'
+    def renameTransform(self, txt):
+        selectionList = mc.ls(selection=True, type='transform')
+        if selectionList:
+            mc.rename(selectionList[-1], txt)
+            print ">> Object New Name : " + txt
+        else:
+            print ">> You need a selection"
+        self.actuSelection()
+
+    def selectByName(self, txt):
+        mc.select(txt)
+
+    def selVertex(self, *args):
+        mc.selectMode(component=True)
+        mc.selectType(vertex=True)
+
+    def selEdge(self, *args):
+        mc.selectMode(component=True)
+        mc.selectType(edge=True)
+
+    def selFace(self, *args):
+        mc.selectMode(component=True)
+        mc.selectType(facet=True)
+
+    def selObject(self, *args):
+        mc.selectMode(object=True)
+
+    def selMulti(self, *args):
+        mc.selectMode(component=True)
+        mc.selectType(meshComponents=True)
+
+    def selUv(self, *args):
+        mc.selectMode(component=True)
+        mc.selectType(polymeshUV=True)
